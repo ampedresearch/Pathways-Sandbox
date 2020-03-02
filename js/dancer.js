c06b85b572f99098e61af5371c5b7ec1a49146b6
Yoav Bergner <ybergner@gmail.com>
2020-03-02 14:56:55 -0500

    Update dancer.js
    bounceback and begin facing

--- js/dancer.js
+++ js/dancer.js
@@ -1,9 +1,10 @@
 class Dancer {
 	constructor(){
 		//position
-		this.pos = createVector(50,50);
+		this.pos = createVector(width/2,height/2);
 		this.direction;
 		this.state;
+		this.facing = createVector(0,1);
 
 		// slider value
 		this.slider = 5; //default to 5
@@ -20,8 +21,6 @@ class Dancer {
 
 		// linear vars
 		this.velocity = createVector(1,0);
-		this.startPos = createVector(100,100);
-		this.endPos = createVector(700,100);
 	}
 
 
@@ -33,6 +32,18 @@ class Dancer {
 			this.radius = this.slider * 18;
 			this.updateCircular();
 		} else if (this.state == "LINEAR"){
+			stroke(255);
+			this.startPos = createVector(width/2 - this.slider * 20,height/2);
+			this.endPos = createVector(width/2 + this.slider * 20,height/2);
+			line(this.startPos.x,this.startPos.y,this.endPos.x,this.endPos.y);
+			fill(0);
+			noStroke();
+			ellipse(this.startPos.x,this.startPos.y, 15, 15);
+			ellipse(this.endPos.x,this.endPos.y, 15, 15);
+			// this.pos.x = this.startPos.x;
+			// this.pos.y = this.startPos.y;
+			//
+
 			this.updateLinear();
 		}
 
@@ -41,7 +52,6 @@ class Dancer {
 
 	updateState(state){
 		this.state = state;
-
 		this.pos.x = this.startPos.x;
 		this.pos.y = this.startPos.y;
 	}
@@ -53,44 +63,48 @@ class Dancer {
 	updateCircular(){
 		this.pos.x = this.center.x + this.radius * cos(this.angle);
 		this.pos.y = this.center.y + this.radius * sin(this.angle);
+		// this.facing.x = -50 * cos(this.angle);
+		// this.facing.y = -50 * sin(this.angle);
+		// this.facing.x = -50 * cos(this.angle);
+		// this.facing.y = -50 * sin(this.angle);
+		// backwards
+		// this.facing.x = 50 * sin(this.angle);
+		// this.facing.y = -50 * cos(this.angle);
+		// forwards
+		this.facing.x = -50 * sin(this.angle);
+		this.facing.y = 50 * cos(this.angle);
+
 
 		this.angle = this.angle + this.speed;
 
-		// draw path below dancer 
+		// draw path below dancer
 		noFill();
 		stroke(255);
 		ellipse(this.center.x, this.center.y, this.radius * 2);
+		line(this.pos.x, this.pos.y,this.pos.x+this.facing.x,this.pos.y+this.facing.y)
 	}
 
 	updateLinear(){
-		// this shouldn't be here!
-		// neeed to fix direction if statements 
-		this.velocity.x = this.slider /2;
-
-		// draw pathway start + end points
-		stroke(255);
-		line(this.startPos.x,this.startPos.y,this.endPos.x,this.endPos.y);
-		fill(0);
-		noStroke();
-		ellipse(this.startPos.x,this.startPos.y, 15, 15);
-		ellipse(this.endPos.x,this.endPos.y, 15, 15);
-
-		if(this.pos.x >= this.endPos.x){ //if at end
-			this.velocity.x = -this.slider /2;
-			// should change this to either change direction OR set velocity to 0?
-		} else if (this.pos.x <= this.startPos.x){
-			this.velocity.x = this.slider /2;
-		}
-
-		// if end pos is X vector distance for start pos
-		//end movement
-
 
 
 		// calculate velocity as vector between start / end positions
 		// temp hard-coded values of velocity
 		this.pos.x += this.velocity.x; // 1
 		this.pos.y += this.velocity.y; // 0
+
+
+
+		//
+		if(this.pos.x > this.endPos.x){ //if at end
+			this.velocity.x = -this.velocity.x;
+			// should change this to either change direction OR set velocity to 0?
+		} else if (this.pos.x < this.startPos.x){
+			this.velocity.x = -this.velocity.x;
+		}
+
+		// if end pos is X vector distance for start pos
+		//end movement
+
 
 
 	}
